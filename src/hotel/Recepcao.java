0d077f97c804fbb61a8ec9a147e211de68461ea7
LUCCA DO O CATAO BONGIOVI 115210757 <luccacb@lcc2-24.lcc.ufcg.edu.br>
2017-06-29 11:36:49 -0300

    Adicionei classe recepção

---
+++ src/hotel/Recepcao.java
@@ -0,0 +1,29 @@
+package hotel;
+
+public class Recepcao {
+	
+	public void checkIn(String nome, String tipo, int idade,int dias, double valor){
+		
+	}
+	
+	public void checkOut(String nome){
+		
+	}
+	
+	int getNumDeHospedes(){
+		
+		
+		
+	}
+	
+	double getLucroTotal(){
+		
+	}
+
+	
+	String toString(){
+		
+	}
+	
+
+}
