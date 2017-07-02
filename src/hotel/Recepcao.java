cedddc1218647cd07671eae4ccac5694aaf55a31
francisandradee <francivaldo.andrade@ccc.ufcg.edu.br>
2017-07-02 18:19:39 -0300

    criei a classe Hotel e o arraylist em recepcao

--- src/hotel/Recepcao.java
+++ src/hotel/Recepcao.java
@@ -1,9 +1,12 @@
 package hotel;
 
+import java.util.ArrayList;
+
 public class Recepcao {
-
+	private ArrayList estadias = new ArrayList();
+	
 	public void checkIn(String nome, String tipo, int idade, int dias, double valor) {
-
+		
 	}
 
 	public void checkOut(String nome) {
